383856ab9daf1e43fcca48b01c8ef5bd998f121e
Diego <diego2.gd@gmail.com>
2018-10-19 23:35:36 +0200

    Comsumer and producer

---
+++ src/SimpleConsumer.java
@@ -0,0 +1,71 @@
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Properties;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+
+
+public class SimpleConsumer {
+
+	Properties props;
+	
+	KafkaConsumer<String, String> consumer;
+	
+	SimpleConsumer(){
+		Properties props = new Properties();
+		
+		props.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+		props.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "Group1");
+		props.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,	StringDeserializer.class.getName());
+		props.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+
+		consumer = new KafkaConsumer<>(props);
+
+	}
+	
+	public void suscribe(String[] topic) {
+		System.out.println("suscribre");
+		List<String> topics = Arrays.asList("SDTF");
+		this.consumer.subscribe(topics);
+	}
+	
+	public void stop() {
+		System.out.println("FIN");
+		this.consumer.close();
+		
+		}
+	
+	 public void consume() {
+		 System.out.println("consume enter");
+		
+		
+		while(true) {
+			 
+			ConsumerRecords<String, String> records = this.consumer.poll(100);
+			
+			for (ConsumerRecord<String, String> record : records) { 
+				System.out.println("part3");
+				System.out.println("something consumed");
+				System.out.printf("offset = %d, key = %s, value = %s%n",
+				record.offset(), record.key(), record.value());
+				}
+		}
+		
+		}
+	
+	public static void main(String[] args) {
+		SimpleConsumer consumerp = new SimpleConsumer();
+		
+		String[] topics = {"consumer-1", "SDTF"};
+		consumerp.suscribe(topics);
+		consumerp.consume();
+		consumerp.stop();
+		
+	}
+	
+}
---
+++ src/SimpleProducer.java
@@ -0,0 +1,44 @@
+
+import java.util.Properties;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+
+public class SimpleProducer {
+
+	Properties props;
+	
+	KafkaProducer<String, String> producer;
+	
+	SimpleProducer(){
+		Properties props = new Properties();
+		
+		props.put("bootstrap.servers", "localhost:9092");
+		props.put("key.serializer","org.apache.kafka.common.serialization.StringSerializer");
+		props.put("value.serializer","org.apache.kafka.common.serialization.StringSerializer");
+		producer = new KafkaProducer<>(props);
+
+	}
+	
+	void produceAndPrint() {
+		for (int i = 1; i < 100; i++)
+		// Fire-and-forget send(topic, key, value)
+		// Send adds records to unsent records buffer and return
+		producer.send(new ProducerRecord<String, String>("SDTF", Integer
+		.toString(i), Integer.toString(i)));
+		}
+	
+	void stop() {
+		System.out.println("FIN");
+		producer.close();
+		
+		}
+	
+	public static void main(String[] args) {
+		SimpleProducer myProducer = new SimpleProducer();
+		myProducer.produceAndPrint();
+		myProducer.stop();
+	}
+
+	
+}
